2b0e495529f9ee92be45c3d4f522382a192c1098
Prince Ikede <princeikede@gmail.com>
2021-12-01 17:16:16 -0500

    Adds 2021 and refactors 2020

---
+++ src/yeartwenty/daynine/DayNineJava.java
@@ -0,0 +1,96 @@
+package yeartwenty.daynine;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+public class DayNineJava {
+    public static void main(String[] args) throws IOException {
+        List<String> inputFile = Files.readAllLines(Paths.get("src/yeartwenty/daynine/file.txt"));
+
+        ExceptionNumber exceptionNumberObj = new ExceptionNumber(inputFile);
+        int exceptionNumber = exceptionNumberObj.getExceptionNumber();
+        System.out.println(exceptionNumber);   // ANS -> 88311122
+        System.out.println(exceptionNumberObj.getEncryptionWeakness(exceptionNumber));    // ANS  ->  13549369
+    }
+}
+
+class ExceptionNumber {
+    ArrayList<String> input;
+
+    public ExceptionNumber(List<String> input) {
+        this.input = (ArrayList<String>) input;
+    }
+
+    int getExceptionNumber() {
+        for (int index = 0; index < input.size(); index++) {
+            boolean seen = false;
+            int currentNumber = index;
+            int end = index + 24;
+            int target = getIntValueForIndex(end + 1);
+            while (currentNumber <= end) {
+                int temp = currentNumber + 1;
+                while (temp <= end) {
+                    if (getIntValueForIndex(temp) + getIntValueForIndex(currentNumber) == target) {
+                        seen = true;
+                    }
+                    temp++;
+                }
+                currentNumber++;
+            }
+            if (!seen) {
+                return target;
+            }
+        }
+
+        return -1;
+    }
+
+    int getEncryptionWeakness(int target) {
+
+        int start = 0, end = 1;
+        int sum;
+
+        for (int index = 0; index < input.size(); index++) {
+            int tempEnd = end;
+            sum =  getIntValueForIndex(start) + getIntValueForIndex(end);
+            while (tempEnd < input.size() && start < tempEnd) {
+                if (sum == target) {
+                    return getMinMaxSum(start, tempEnd);
+                } else if (sum > target) {
+                    // reduce window by moving window's starting position
+                    sum -= getIntValueForIndex(start++);
+                } else {
+                    // increase window by moving window's end position
+                    tempEnd++;
+                    sum += getIntValueForIndex(tempEnd);
+                }
+            }
+            end = tempEnd;
+        }
+        return -1;
+    }
+
+    // returns sum of max number and minimum number in contiguous set
+    int getMinMaxSum(int start, int end) {
+        int min = Integer.MAX_VALUE, max = Integer.MIN_VALUE;
+
+        for (int i = start; i <= end; i++) {
+            int currentIndex = getIntValueForIndex(i);
+            if (min > currentIndex) {
+                min = currentIndex;
+            }
+            if (max < currentIndex) {
+                max = currentIndex;
+            }
+        }
+
+        return min + max;
+    }
+
+    private int getIntValueForIndex(int index) {
+        return Integer.parseInt(input.get(index));
+    }
+}
---
+++ src/yeartwentyone/DayOne.kt
@@ -0,0 +1,55 @@
+package yeartwentyone
+
+import java.nio.file.Files
+import java.nio.file.Paths
+
+fun main() {
+    val input = Files.readAllLines(Paths.get("src/yeartwentyone/file.txt"))
+    println(partOne(input))   // 1548
+    println(partTwo(input))   // 1589
+}
+
+fun partTwo(input: List<String>): Int {
+    var ansCnt = 0
+    var index = 0
+    val size = input.size
+    var previousSum = 0
+    while (index < size){
+        if (index <= 2){
+            previousSum += input[index].toInt()
+            index++
+            continue
+        }
+        var nextSum = 0
+        var start = index
+        var end = 2 + index
+//        while(start <= end && end < size){   OR
+        while(end in start until size){
+            nextSum += input[start++].toInt()
+        }
+        if (previousSum < nextSum)
+            ansCnt++
+        previousSum = nextSum
+        index++
+    }
+
+    return ansCnt
+}
+
+fun partOne(input: List<String>): Int {
+    var ansCnt = 0
+    var index = 0
+    val size = input.size
+
+    while (index < size) {
+        if (index == 0){
+            index++
+            continue
+        }
+        if( input[index].toInt() > input[index - 1 ].toInt()){
+            ansCnt++
+        }
+        index++
+    }
+    return ansCnt
+}
