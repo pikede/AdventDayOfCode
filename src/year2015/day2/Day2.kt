9a0eb5a1d9e06c0bfb0ca1b24654767a9fe0391d
Prince Ikede <21118162+pikede@users.noreply.github.com>
2024-03-22 22:03:07 -0400

    creates day 2 files: will be solved later (#41)
    * creates day 2 files: will be solved later
    
    * day 2 2015
    
    ---------
    
    Co-authored-by: Prince Ikede <princeikede@gmail.com>

---
+++ src/year2015/day2/Day2.kt
@@ -0,0 +1,40 @@
+package year2015.day2
+
+import AOCPuzzle
+import java.nio.file.Files
+import java.nio.file.Paths
+
+private val questionInput: MutableList<String> = Files.readAllLines(Paths.get("src/year2015/day2/file.txt"))
+
+fun main() {
+    val solution = Day2Solution(questionInput)
+    println(solution.part1())
+    println(solution.part2())
+}
+
+class Day2Solution(private val puzzleInput: MutableList<String>) : AOCPuzzle {
+    private val boxesDimensions by lazy {
+        puzzleInput.map { dimension ->
+            dimension.split("x").map { it.toInt() }
+        }
+    }
+
+    override fun part1(): Any {
+        return boxesDimensions.fold(0) { totalSurfaceArea, currentBox ->
+            val (l, w, h) = currentBox
+            val areaOfSmallestSide = minOf(h * l, l * w, w * h)
+            val currentSurfaceArea = areaOfSmallestSide + (2 * l * w) + (2 * w * h) + (2 * h * l)
+            currentSurfaceArea + totalSurfaceArea
+        }
+    }
+
+    override fun part2(): Any {
+        return boxesDimensions.fold(0) { totalRibbonLength, currentBox ->
+            val (l, w, h) = currentBox
+            val boxVolume = l * w * h
+            val smallestBoxPerimeter = minOf(2 * (l + w), 2 * (l + h), 2 * (h + w))
+            val currentRibbonLength = boxVolume + smallestBoxPerimeter
+            currentRibbonLength + totalRibbonLength
+        }
+    }
+}
