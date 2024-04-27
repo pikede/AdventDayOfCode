c94a00cd54755db610759af29943b9b7004fde35
Prince Ikede <princeikede@gmail.com>
2024-04-26 21:02:40 -0400

    day 18 2015

---
+++ src/year2015/day18/Day18.kt
@@ -0,0 +1,106 @@
+package year2015.day18
+
+import AOCPuzzle
+import java.nio.file.Files
+import java.nio.file.Paths
+
+private val quizInput: MutableList<String> = Files.readAllLines(Paths.get("src/year2015/day18/file.txt"))
+private const val NUMBER_OF_STEPS = 100
+
+private fun main() {
+    val solution = Day18Solution()
+    println(solution.part1())
+    println(solution.part2())
+}
+
+private class Day18Solution : AOCPuzzle {
+    @OptIn(ExperimentalStdlibApi::class)
+    var initial = buildList {
+        quizInput.forEach {
+            add(it.toMutableList())
+        }
+    }.toMutableList()
+
+    override fun part1(): Any {
+        var copy = ArrayList(initial)
+        repeat(NUMBER_OF_STEPS) {
+            val result = ArrayList<MutableList<Char>>()
+            for (r in copy.indices) {
+                val level = ArrayList<Char>()
+                for (c in copy[r].indices) {
+                    level += when (copy[r][c]) {
+                        '#' -> {
+                            if (countOnNeighbors(copy, r, c) in 2..3) {
+                                '#'
+                            } else {
+                                '.'
+                            }
+                        }
+                        else -> { //'.' dot scenario
+                            if (countOnNeighbors(copy, r, c) == 3) {
+                                '#'
+                            } else {
+                                '.'
+                            }
+                        }
+                    }
+                }
+                result += ArrayList(level)
+            }
+            copy = result
+        }
+        return copy.sumBy { it.count { light -> light == '#' } }
+    }
+
+    override fun part2(): Any {
+        var copy = ArrayList(initial)
+        val n = copy.size
+        val m = copy[0].size
+        val corners = hashSetOf(0 to 0, n - 1 to 0, 0 to m - 1, n - 1 to m - 1)
+        for ((r, c) in corners) {
+            copy[r][c] = '#'
+        }
+        repeat(NUMBER_OF_STEPS) {
+            val result = ArrayList<MutableList<Char>>()
+            for (r in copy.indices) {
+                val level = ArrayList<Char>()
+                for (c in copy[r].indices) {
+                    level += when {
+                        r to c in corners -> '#'
+                        copy[r][c] == '#' -> {
+                            if (countOnNeighbors(copy, r, c) in 2..3) {
+                                '#'
+                            } else {
+                                '.'
+                            }
+                        }
+                        copy[r][c] == '.' -> { //'.' dot scenario
+                            if (countOnNeighbors(copy, r, c) == 3) {
+                                '#'
+                            } else {
+                                '.'
+                            }
+                        }
+                        else -> throw IllegalArgumentException("Invalid character encountered at row: $r col: $c")
+                    }
+                }
+                result += ArrayList(level)
+            }
+            copy = result
+        }
+        return copy.sumBy { it.count { light -> light == '#' } }
+    }
+
+    private fun countOnNeighbors(grid: ArrayList<MutableList<Char>>, row: Int, col: Int): Int {
+        val directions = listOf(-1 to -1, -1 to 0, -1 to 1, 0 to -1, 0 to 1, 1 to -1, 1 to 0, 1 to 1)
+        var count = 0
+        for ((y, x) in directions) {
+            val r = y + row
+            val c = x + col
+            if (r in grid.indices && c in grid[r].indices && grid[r][c] == '#') {
+                count++
+            }
+        }
+        return count
+    }
+}
