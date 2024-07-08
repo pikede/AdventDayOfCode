eaa8a12023168804cd5872d7c583ca756110de98
Prince Ikede <21118162+pikede@users.noreply.github.com>
2024-07-08 18:37:48 +0100

    day 20 with cleanup for 18 & 19 (#60)
    Co-authored-by: Prince Ikede <princeikede@gmail.com>

---
+++ src/year2015/day20/Day20.kt
@@ -0,0 +1,36 @@
+package year2015.day20
+
+import AOCPuzzle
+
+private const val TARGET_PRESENTS = 33100000
+
+private fun main() {
+    val day18 = Day18Solution
+    println(day18.part1())
+    println(day18.part2())
+}
+
+private object Day18Solution : AOCPuzzle {
+
+    override fun part1(): Any {
+        return getLowestHouseNumber( 10, Int.MAX_VALUE)
+    }
+
+    override fun part2(): Any {
+        return getLowestHouseNumber(11, 50)
+    }
+
+    private fun getLowestHouseNumber(increments: Int, houseLimit: Int): Int {
+        val d = TARGET_PRESENTS / increments
+        val counts = MutableList(d) { 0 }
+        for (i in 1..d) {
+            for (j in 1..minOf(d / i, houseLimit)) {
+                counts[j * i - 1] += i * increments
+            }
+            if (counts[i - 1] >= TARGET_PRESENTS) {
+                return i
+            }
+        }
+        return -1
+    }
+}
