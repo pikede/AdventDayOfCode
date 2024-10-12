6589677a9445ed8bd0e62a84e89377b231b3a65e
Prince Ikede <princeikede@gmail.com>
2024-10-12 15:05:04 -0400

    part 1

--- src/year2015/day23/Day23.kt
+++ src/year2015/day23/Day23.kt
@@ -7,12 +7,12 @@ import java.nio.file.Paths
 private val quizInput: MutableList<String> = Files.readAllLines(Paths.get("src/year2015/day23/file.txt"))
 
 private fun main() {
-    val day22 = Day22Solution
-    println(day22.part1())
-    println(day22.part2())
+    val day23 = Day23Solution
+    println(day23.part1())
+    println(day23.part2())
 }
 
-private object Day22Solution : AOCPuzzle {
+private object Day23Solution : AOCPuzzle {
     override fun part1() = getFinalValueForRegisterB(aRegisterIntialValue = 0, bRegisterInitialValue = 0)
 
     override fun part2() = getFinalValueForRegisterB(aRegisterIntialValue = 1L, bRegisterInitialValue = 0L)
@@ -24,10 +24,11 @@ private object Day22Solution : AOCPuzzle {
         while (index in quizInput.indices) {
             val instruction = quizInput[index]
             val split = instruction.replace(",", "").split(" ")
+            val currentRegister = split[1]
 
             index += when (split[0]) {
                 "hlf" -> {
-                    if (split[1] == "a") {
+                    if (currentRegister == "a") {
                         registerA /= 2
                     } else {
                         registerB /= 2
@@ -35,7 +36,7 @@ private object Day22Solution : AOCPuzzle {
                     1
                 }
                 "tpl" -> {
-                    if (split[1] == "a") {
+                    if (currentRegister == "a") {
                         registerA *= 3
                     } else {
                         registerB *= 3
@@ -43,18 +44,18 @@ private object Day22Solution : AOCPuzzle {
                     1
                 }
                 "inc" -> {
-                    if (split[1] == "a") registerA++ else registerB++
+                    if (currentRegister == "a") registerA++ else registerB++
                     1
                 }
-                "jmp" -> split[1].toInt()
+                "jmp" -> currentRegister.toInt()
                 "jie" -> {  // if register is even
-                    val register = if (split[1] == "a") registerA else registerB
+                    val register = if (currentRegister == "a") registerA else registerB
                     if (register % 2 == 0L) {
                         split[2].toInt()
                     } else 1
                 }
                 "jio" -> {  // if register is 1
-                    val register = if (split[1] == "a") registerA else registerB
+                    val register = if (currentRegister == "a") registerA else registerB
                     if (register == 1L) {
                         split[2].toInt()
                     } else 1
