eb8755c4b15f68dcd88c7e504ee38f991410e00c
Prince Ikede <princeikede@gmail.com>
2024-10-18 23:33:47 -0400

    day 25 part 1

---
+++ src/year2015/day25/Day25.kt
@@ -0,0 +1,52 @@
+package year2015.day24
+
+import AOCPuzzle
+import java.nio.file.Files
+import java.nio.file.Paths
+
+private val quizInput: MutableList<String> = Files.readAllLines(Paths.get("src/year2015/day25/file.txt"))
+
+private fun main() {
+    val day25 = Day25Solution
+    println(day25.part1())
+//    println(day25.part2())
+    println((33511524 * 252533L) % 33554393)
+}
+
+private object Day25Solution : AOCPuzzle {
+    val grid = Array(10000) { LongArray(10000) }
+
+    override fun part1(): Any {
+        var currentRow = 1
+        var currentValue = 20151125L
+        grid[0][0] = currentValue
+        while (grid[2981][3075] == 0L) {
+            for (c in 0..currentRow) {
+                val nextValue = (currentValue * 252533L) % 33554393L
+                currentValue = nextValue
+                if (currentRow - c in grid.indices && c in grid[0].indices) {
+                    grid[currentRow - c][c] = currentValue
+                } else {
+                    break
+                }
+            }
+            currentRow++
+        }
+
+//        grid.forEach { array ->
+//            array.forEach {
+//                if (it != 0L) {
+//                    print("$it, ")
+//                }
+//            }
+//            println()
+//        }
+        val row = 2981
+        val column = 3075
+        return grid[row - 1][column - 1]
+    }
+
+    override fun part2(): Any {
+        return 0
+    }
+}
