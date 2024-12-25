19350f5379b62a73fbf7b09e04c389b3025c9bfb
Prince Ikede <21118162+pikede@users.noreply.github.com>
2024-12-25 00:56:27 -0500

    2024 day 25 (#66)
    * part 1
    
    * day 25 part 1
    
    ---------
    
    Co-authored-by: Prince Ikede <princeikede@gmail.com>

---
+++ src/twenty4/day25/Day25.kt
@@ -0,0 +1,66 @@
+package twenty4.day25
+
+import java.nio.file.Files
+import java.nio.file.Paths
+
+private val quizInput: MutableList<String> = Files.readAllLines(Paths.get("src/twenty4/day25/file.txt"))
+val locks = mutableListOf<Grid>()
+val keys = mutableListOf<Grid>()
+private fun main() {
+    parse()
+    println(part1())
+}
+
+private fun part1(): Int {
+    var count = 0
+    for (key in keys) {
+        for (lock in locks) {
+            if (isMatch(key, lock)) {
+                count++
+            }
+        }
+    }
+    return count
+}
+
+private fun isMatch(key: Grid, lock: Grid): Boolean {
+    for (index in key.heights.indices) {
+        if (key.heights[index] + lock.heights[index] >= 6) {
+            return false
+        }
+    }
+    return true
+}
+
+private fun parse() {
+    val grid = mutableListOf<String>()
+    for (i in quizInput) {
+        if (i.isEmpty()) {
+            val isLock = grid[0].all { it == '#' }
+            val tempGrid = Grid(grid, isLock)
+            if (isLock) locks += tempGrid else keys += tempGrid
+            grid.clear()
+            continue
+        }
+        grid.add(i)
+    }
+    val isLock = grid[0].all { it == '#' }
+    val tempGrid = Grid(grid.toMutableList(), isLock)
+    if (isLock) locks += tempGrid else keys += tempGrid
+}
+// lock top row == #
+// key bottom row == ##
+
+data class Grid(val matrix: MutableList<String>, var isLock: Boolean) {
+    val heights = MutableList(matrix[0].length) { 0 }
+
+    init {
+        for (r in matrix.indices) {
+            for (c in matrix[r].indices) {
+                if (isLock && r == 0) continue
+                if (!isLock && r == matrix.lastIndex) continue
+                heights[c] += if (matrix[r][c] == '#') 1 else 0
+            }
+        }
+    }
+}
