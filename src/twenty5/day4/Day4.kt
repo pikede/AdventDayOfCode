953d2685794cb6cec45d3b73aa7b2e10e6a920cf
Prince Ikede <davidikede24@gmail.com>
2025-12-04 11:57:36 -0500

    day 4 part 1

---
+++ src/twenty5/day4/Day4.kt
@@ -0,0 +1,39 @@
+package twenty5.day3
+
+import AOCPuzzle
+import utils.Direction
+import java.nio.file.Files
+import java.nio.file.Paths
+
+private val quizInput: MutableList<String> = Files.readAllLines(Paths.get("src/twenty5/day4/file.txt"))
+
+private fun main() {
+    println(Day4.part1()) // 1435
+    println(Day4.part2())
+}
+
+private object Day4 : AOCPuzzle {
+
+    override fun part1(): Int {
+        var count = 0
+        for (r in quizInput.indices) {
+            for (c in quizInput[r].indices) {
+                if(quizInput[r][c] != '@') continue
+                var currentRollsOfPaper = 0
+                for (direction in Direction.all8()) {
+                    val newR = r + direction.y
+                    val newC = c + direction.x
+                    if (newR in quizInput.indices && newC in quizInput[newR].indices && quizInput[newR][newC] == '@') {
+                        currentRollsOfPaper++
+                    }
+                }
+                if (currentRollsOfPaper < 4) count++
+            }
+        }
+        return count
+    }
+
+    override fun part2(): Long {
+        return 0
+    }
+}
