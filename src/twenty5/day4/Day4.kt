3e55aaee1d2ed660af9ef904018fa34c273a3bcf
Prince Ikede <davidikede24@gmail.com>
2025-12-04 12:04:14 -0500

    day 4 part 2

--- src/twenty5/day4/Day4.kt
+++ src/twenty5/day4/Day4.kt
@@ -9,7 +9,7 @@ private val quizInput: MutableList<String> = Files.readAllLines(Paths.get("src/t
 
 private fun main() {
     println(Day4.part1()) // 1435
-    println(Day4.part2())
+    println(Day4.part2()) // 8623
 }
 
 private object Day4 : AOCPuzzle {
@@ -33,7 +33,35 @@ private object Day4 : AOCPuzzle {
         return count
     }
 
-    override fun part2(): Long {
-        return 0
+    override fun part2(): Int {
+        var count = 0
+        val toRemove = mutableSetOf<Pair<Int, Int>>()
+        val quizInputArr = quizInput.map { it.toCharArray() }.toMutableList()
+        do {
+            toRemove.forEach {
+                val (r, c) = it
+                quizInputArr[r][c] = '.'
+            }
+            toRemove.clear()
+            for (r in quizInputArr.indices) {
+                for (c in quizInputArr[r].indices) {
+                    if (quizInputArr[r][c] != '@') continue
+                    var currentRollsOfPaper = 0
+                    for (direction in Direction.all8()) {
+                        val newR = r + direction.y
+                        val newC = c + direction.x
+                        if (newR in quizInputArr.indices && newC in quizInputArr[newR].indices && quizInputArr[newR][newC] == '@') {
+                            currentRollsOfPaper++
+                        }
+                    }
+                    if (currentRollsOfPaper < 4) {
+                        count++
+                        toRemove += r to c
+                    }
+                }
+            }
+
+        } while (toRemove.isNotEmpty())
+        return count
     }
 }
