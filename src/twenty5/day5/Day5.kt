41d9fc29df1fdff948bb242b1f53fb62ed87bf75
Prince Ikede <davidikede24@gmail.com>
2025-12-05 13:10:20 -0500

    cleanup part 2 day 5

--- src/twenty5/day5/Day5.kt
+++ src/twenty5/day5/Day5.kt
@@ -1,31 +1,27 @@
 package twenty5.day5
 
 import AOCPuzzle
-import java.math.BigInteger
 import java.nio.file.Files
 import java.nio.file.Paths
-import kotlin.math.sign
 
 private val quizInput: MutableList<String> = Files.readAllLines(Paths.get("src/twenty5/day5/file.txt"))
 
 private fun main() {
-    println(Day5.part1())        // 525
-    println(Day5.part2())       // 333892124923577
+    println(Day5.part1())   // 525
+    println(Day5.part2())   // 333892124923577
 }
 
 
 private object Day5 : AOCPuzzle {
-
-    private data class IDRange(var start: Long, var end: Long) {
-        val freshIdRangeCount = (end - start) + 1
-        fun isIDWithinRange(id: Long) = id in start..end
-    }
-
-    private val ranges = mutableListOf<IDRange>()
+    private val ranges = mutableListOf<LongRange>()
     private val ids = mutableListOf<Long>()
-    private var isRange = true
 
     init {
+        getRangesAndIDs()
+    }
+
+    private fun getRangesAndIDs() {
+        var isRange = true
         for (input in quizInput) {
             if (input.isEmpty()) {
                 isRange = false
@@ -34,7 +30,7 @@ private object Day5 : AOCPuzzle {
             when {
                 isRange -> {
                     val (start, end) = input.split("-").map { it.toLong() }
-                    ranges.add(IDRange(start, end))
+                    ranges.add(LongRange(start, end))
                 }
 
                 else -> {
@@ -47,38 +43,28 @@ private object Day5 : AOCPuzzle {
     override fun part1(): Long {
         var count = 0L
         for (id in ids) {
-            for (range in ranges) {
-                if (range.isIDWithinRange(id)) {
-                    count++
-                    break
-                }
+            if (ranges.any { id in it }) {
+                count++
             }
         }
         return count
     }
 
     override fun part2(): Long {
-        ranges.sortWith(compareBy { it.start })
-        val mergedRanges = mutableListOf<IDRange>()
-        mergedRanges.add(ranges.first())
-
-        for (i in 1..ranges.lastIndex) {
-            val last = mergedRanges.last()
-            val current = ranges[i]
-            if (last.end >= current.start) {
-                val start = minOf(last.start, current.start)
-                val end = maxOf(current.end, last.end)
-                mergedRanges[mergedRanges.lastIndex] = IDRange(start, end)
-            } else {
-                mergedRanges.add(current)
+        return ranges
+            .distinctBy { it.first..it.last } // removes duplicate Id ranges
+            .sortedBy { it.first }  // sorts by starting range
+            .fold(emptyList<LongRange>()) { acc, nextRange -> // merges the overlapping ranges
+                val lastRange = acc.lastOrNull()
+                when {
+                    lastRange == null -> listOf(nextRange)
+                    lastRange.last < nextRange.first -> acc + listOf(nextRange)
+                    else -> {
+                        acc.dropLast(1) + listOf(lastRange.first..maxOf(lastRange.last, nextRange.last))
+                    }
+                }
+            }.fold(0L) { acc, next -> // using fold here as a workaround for sumBy
+                acc + next.last - next.first + 1
             }
-        }
-
-        var count = 0L
-        mergedRanges.forEach {
-            count += it.freshIdRangeCount
-        }
-        return count
-
     }
 }
