088e0022451e0ee8c86c9b9bb92e78de9ea7f056
Prince Ikede <davidikede24@gmail.com>
2025-12-06 06:37:43 -0500

    day 6 part 1

--- src/twenty5/day6/Day6.kt
+++ src/twenty5/day6/Day6.kt
@@ -1,4 +1,4 @@
-package twenty5.day5
+package twenty5.day6
 
 import AOCPuzzle
 import java.nio.file.Files
@@ -7,16 +7,41 @@ import java.nio.file.Paths
 private val quizInput: MutableList<String> = Files.readAllLines(Paths.get("src/twenty5/day6/file.txt"))
 
 private fun main() {
-    println(Day6.part1())
+    println(Day6.part1())   /// 4693159084994
     println(Day6.part2())
+
 }
 
 private object Day6 : AOCPuzzle {
-    override fun part1(): Int {
-        return 0
+    override fun part1(): Long {
+        val numbers = quizInput
+            .takeWhile { !it.hasSign() }
+            .map {
+                it.split(" ")
+                    .filter { it.isNotEmpty() }
+                    .map { it.toInt() }
+            }
+        val signs = quizInput.first { it.hasSign() }.split(" ").filter { it.isNotEmpty() }
+
+        var total = 0L
+        signs.forEachIndexed { index, sign ->
+            var current = if (sign == "+") 0L else 1L
+            when {
+                sign == "+" -> numbers.onEach { current += it[index] }
+                else -> numbers.onEach { current *= it[index] }
+            }
+            total += current
+        }
+        return total
     }
 
     override fun part2(): Long {
         return 0
     }
+
+    private fun String.isInt(): Boolean {
+        return runCatching { this.toInt() is Int }.onSuccess { true }.onFailure { false }.getOrNull() ?: false
+    }
+
+    private fun String.hasSign() = '+' in this || '*' in this
 }
