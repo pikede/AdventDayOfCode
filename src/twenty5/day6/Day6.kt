dc63b63005acb966b21b7d42b4635476db844bac
Prince Ikede <davidikede24@gmail.com>
2025-12-07 18:11:06 -0500

    day 6 1 & 2

--- src/twenty5/day6/Day6.kt
+++ src/twenty5/day6/Day6.kt
@@ -8,8 +8,7 @@ private val quizInput: MutableList<String> = Files.readAllLines(Paths.get("src/t
 
 private fun main() {
     println(Day6.part1())   /// 4693159084994
-    println(Day6.part2())
-
+    println(Day6.part2())  // 11643736116335
 }
 
 private object Day6 : AOCPuzzle {
@@ -35,13 +34,139 @@ private object Day6 : AOCPuzzle {
         return total
     }
 
-    override fun part2(): Long {
-        return 0
+   override fun part2(): Long {
+        val splits: List<IntRange> =
+            quizInput
+                .last()
+                .foldIndexed(emptyList<Int>()) { index, acc, s ->
+                    when {
+                        s == ' ' -> acc
+                        else -> acc + maxOf(0, index)
+                    }
+                }
+                .zipWithNext { a, b -> a until b - 1 }
+                .let {
+                    it + listOf(
+                        it.last().last + 2 until quizInput.maxBy(String::length)!!.length,
+                    )
+                }
+
+        val operations =
+            quizInput
+                .last()
+                .split(" ")
+                .filter(String::isNotEmpty)
+                .map(String::trim)
+
+        // transpose, get all numbers in same column
+        // for each row, transpose again but for the digits + pad
+        val result =
+            quizInput
+                .dropLast(1)
+                .map { line ->
+                    val padded = line.padEnd(splits.last().last + 1, ' ')
+                    splits.map { range -> padded.substring(range) }
+                }
+                .transpose()
+                .map {
+                    // transpose to get the digits
+                    it
+                        .transposeString()
+                        .map(String::trim)
+                        .map(String::toLong)
+                }
+                .withIndex()
+
+                .sumBy { (index, row) ->
+                    when (operations[index]) {
+                        "+" -> row.sum()
+                        "*" -> row.multiply()
+                        else -> error("Unknown operation")
+                    }
+                }
+
+        return result
     }
 
-    private fun String.isInt(): Boolean {
-        return runCatching { this.toInt() is Int }.onSuccess { true }.onFailure { false }.getOrNull() ?: false
+    inline fun <T> Iterable<T>.sumBy(selector: (T) -> Long): Long {
+        var sum = 0L
+        for (element in this) {
+            sum += selector(element)
+        }
+        return sum
     }
+
+    fun List<Long>.multiply(): Long = fold(1L) { acc, n -> acc * n }
+
+    fun <T> List<List<T>>.transpose(): List<List<T>> =
+        first().indices.map { i ->
+            (indices).map { j ->
+                this[j][i]
+            }
+        }
+
+    fun List<String>.transposeString(): List<String> =
+        first().indices.map { i ->
+            (indices)
+                .map { j -> this[j][i] }
+                .joinToString("")
+        }
+
+  /*  @OptIn(ExperimentalStdlibApi::class)
+    override fun part2(): Long {
+        val numbers = quizInput
+            .takeWhile { !it.hasSign() }
+            .map {
+                it.split(" ")
+                    .filter { it.isNotEmpty() }
+                    .map { it.toInt() }
+            }
+        val signs = quizInput.first { it.hasSign() }.split(" ").filter { it.isNotEmpty() }
+
+        var total = 0L
+        signs.forEachIndexed { index, sign ->
+            var currentTotal = if (sign == "+") 0L else 1L
+
+            when {
+                sign == "+" -> {
+                    val column = buildList {
+                        numbers.forEach { add(it[index].toString().reversed().toInt()) }
+                    }
+                    val max = column.maxBy { it.toString().length }?.toString()?.length ?: throw Exception("Invalid 1")
+                    val array = MutableList(max) { 0 }
+                    for (i in column.map { it.toString() }) {
+                        var position = max - 1
+                        for (j in i.indices.reversed()) {
+                            array[position] = (array[position] * 10) + (i[j] - '0')
+                            position--
+                        }
+                    }
+                    print(array)
+                    array.onEach { currentTotal += it }
+                    currentTotal = array.sum().toLong()
+                }
+
+                else -> {
+                    val column = buildList {
+                        numbers.forEach { add(it[index]) }
+                    }
+
+                    val max = column.maxBy { it.toString().length }?.toString()?.length ?: throw Exception("Invalid 1")
+                    val array = MutableList(max) { 0 }
+                    for (i in column.map { it.toString() }) {
+                        for ((position, j) in i.indices.withIndex()) {
+                            array[position] = (array[position] * 10) + (i[j] - '0')
+                        }
+                    }
+                    array.filter { it > 0 }.onEach { currentTotal *= it }
+                }
+            }
+            println(" ===> $currentTotal   $sign")
+            total += currentTotal
+        }
+        return total
+    } // too high 11648318079248
+        */
 
     private fun String.hasSign() = '+' in this || '*' in this
 }
