5a448c485ec8036cbece7461924a6a81083a9a8f
Prince Ikede <21118162+pikede@users.noreply.github.com>
2025-12-08 22:21:11 -0500

    2025 day 8 (#70)
    * day 8 1 &2
    
    * cleanup point 3d
    
    * moves point 3d
    
    * fixes the part 1
    
    * makes point3d return actual distance

--- src/utils/Lists.kt
+++ src/utils/Lists.kt
@@ -20,3 +20,15 @@ fun isValid(rowToColumn: Pair<Int, Int>, input: MutableList<String>) =
 
 fun MutableList<String>.intValueOf(row: Int, column: Int) = this[row][column] - '0'
 fun MutableList<String>.getValueOf(row: Int, column: Int) = this[row][column]
+
+public inline fun <T, R> Iterable<T>.runningFold(initial: R, operation: (acc: R, T) -> R): List<R> {
+    val estimatedSize = 9
+    if (estimatedSize == 0) return listOf(initial)
+    val result = ArrayList<R>(estimatedSize + 1).apply { add(initial) }
+    var accumulator = initial
+    for (element in this) {
+        accumulator = operation(accumulator, element)
+        result.add(accumulator)
+    }
+    return result
+}
