47130fe9589efb297bdd682664b8d56a965c5e90
Prince Ikede <davidikede24@gmail.com>
2025-12-10 11:36:16 -0500

    day 10 boiler plate

---
+++ src/twenty5/day10/Day10.kt
@@ -0,0 +1,23 @@
+package twenty5.day10
+
+import AOCPuzzle
+import java.nio.file.Files
+import java.nio.file.Paths
+
+private val quizInput: MutableList<String> = Files.readAllLines(Paths.get("src/twenty5/day10/file.txt"))
+
+private fun main() {
+    println(Day10.part1())
+    println(Day10.part2())
+}
+
+private object Day10 : AOCPuzzle {
+
+    override fun part1(): Any {
+        return 0L
+    }
+
+    override fun part2(): Any {
+        return 0L
+    }
+}
