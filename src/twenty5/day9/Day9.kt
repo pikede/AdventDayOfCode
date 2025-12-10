b366ab73fa51aef0ae8650f190aab0ee6c76fcc8
Prince Ikede <davidikede24@gmail.com>
2025-12-10 11:33:14 -0500

    day 9 cleanup extra

--- src/twenty5/day9/Day9.kt
+++ src/twenty5/day9/Day9.kt
@@ -3,39 +3,73 @@ package twenty5.day9
 import AOCPuzzle
 import utils.Point2D
 import utils.Point3D
+import utils.maxOf
 import utils.runningFold
 import java.nio.file.Files
 import java.nio.file.Paths
 import kotlin.math.abs
+import kotlin.math.max
+import kotlin.math.min
 
 private val quizInput: MutableList<String> = Files.readAllLines(Paths.get("src/twenty5/day9/file.txt"))
 
 private fun main() {
     println(Day9.part1()) // 4776487744
-    println(Day9.part2())
+    println(Day9.part2()) // 1560299548
 }
 
 private object Day9 : AOCPuzzle {
 
-    override fun part1(): Any {
-        val points = quizInput.map {
-            it.split(',').map { it.toInt() }.let {
-                val (x, y) = it
-                Point2D(x, y)
+    private val points = quizInput.map {
+        it.split(',').map { it.toInt() }.let {
+            val (x, y) = it
+            Point2D(x, y)
+        }
+    }
+
+    @OptIn(ExperimentalStdlibApi::class)
+    val cornerPairs = buildList {
+        (0..points.lastIndex).flatMap { i ->
+            (i + 1..points.lastIndex).map { j ->
+                add(points[i] to points[j])
             }
         }
+    }
+
+    override fun part1(): Any {
         var max = 0L
         (0..points.lastIndex).flatMap { i ->
             (i + 1..points.lastIndex).map { j ->
-                val xDistance = abs(points[i].x - points[j].x) + 1
-                val yDistance = abs(points[i].y - points[j].y) + 1
-                max = maxOf(xDistance.toLong() * yDistance.toLong(), max)
+                val points = (points[i] to points[j])
+                max = maxOf(points.area(), max)
             }
         }
         return max
     }
 
+    private val area: Pair<Point2D, Point2D>.() -> Long = {
+        val (point1, point2) = this
+        val xDistance = abs(point1.x - point2.x) + 1
+        val yDistance = abs(point1.y - point2.y) + 1
+        xDistance.toLong() * yDistance.toLong()
+    }
+
     override fun part2(): Any {
-        return 0
+        fun Pair<Point2D, Point2D>.toSortedEdgePoints(): Pair<Point2D, Point2D> {
+            val (p1, p2) = this
+            val point1 = Point2D(min(p1.x, p2.x), min(p1.y, p2.y))
+            val point2 = Point2D(max(p1.x, p2.x), max(p1.y, p2.y))
+            return point1 to point2
+        }
+
+        val polygonEdges = (points + points.first())
+            .zipWithNext()
+            .map { it.toSortedEdgePoints() }
+
+        return cornerPairs.map { it.toSortedEdgePoints() }.filter { (p1, p2) ->
+            polygonEdges.none { (e1, e2) ->
+                p1.x < e2.x && p2.x > e1.x && p1.y < e2.y && p2.y > e1.y
+            }
+        }.maxOf { it.area() }
     }
 }
