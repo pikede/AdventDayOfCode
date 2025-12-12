b7e0d335b7cde002aab9596c39ae2c9327b91c7a
Prince Ikede <davidikede24@gmail.com>
2025-12-11 20:45:22 -0500

    boilerplate for day 12

---
+++ app/src/main/kotlin/twenty5/day12/Day12.kt
@@ -0,0 +1,23 @@
+package twenty5.day12
+
+import AOCPuzzle
+import org.aoc.utils.readInput
+
+private val quizInput: List<String> = readInput("twenty5/day12/file")
+
+private fun main() {
+    println(Day12.part1())
+    println(Day12.part2())
+}
+
+private object Day12 : AOCPuzzle {
+
+    override fun part1(): Any {
+        return 0
+    }
+
+    override fun part2(): Any {
+        return 0
+    }
+}
+
