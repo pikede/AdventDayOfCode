87495b8d6a42730e5d4045c9b5915f22945da526
Prince Ikede <davidikede24@gmail.com>
2025-12-12 19:23:57 -0500

    day 12

--- app/src/main/kotlin/twenty5/day12/Day12.kt
+++ app/src/main/kotlin/twenty5/day12/Day12.kt
@@ -6,18 +6,46 @@ import org.aoc.utils.readInput
 private val quizInput: List<String> = readInput("twenty5/day12/file")
 
 private fun main() {
-    println(Day12.part1())
+    println(Day12.part1())  // 534
     println(Day12.part2())
 }
 
 private object Day12 : AOCPuzzle {
+    val shapes = quizInput.chunked(5).take(6).map {
+        val shape = it.drop(1).dropLast(1) // drops 0: and empty space
+        Shape(shape)
+    }
+
+    val regions = quizInput.filter { it.any { it == 'x' } }.map {
+        val (size, qty) = it.split(":")
+        val (x, y) = size.split("x").map { it.toInt() }
+        val quantity = qty.split(" ").filterNot { it.isEmpty() }.map { it.toInt() }
+        Regions(x, y, quantity)
+    }
 
     override fun part1(): Any {
-        return 0
+        return regions.count { region ->
+            var spaceLeft = region.area
+            region.quantities.forEachIndexed { index, i ->
+                if (i != 0) {
+                    val spaceConsumed = shapes[index].spaceConsumed * i
+                    spaceLeft -= spaceConsumed
+                }
+            }
+            spaceLeft >= 0
+        }
     }
 
     override fun part2(): Any {
         return 0
+    }
+
+    data class Regions(val width: Int, val length: Int, val quantities: List<Int>) {
+        val area = width * length
+    }
+
+    data class Shape(val shape: List<String>) {
+        val spaceConsumed = shape.sumOf { line -> line.count { it == '#' } }
     }
 }
 
